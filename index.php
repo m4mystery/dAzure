6576acd5038587f47c09e0f38c115a67c8f91d34
m4mystery <mwachala92@hotmail.com>
2017-04-06 17:47:55 +0100

    Skeleton HTML
    Begun trying to dynamically add the code for the web service

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
 
-echo("hello world");
-
-
 ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>title</title>
+  </head>
+  <body>
+  <div id="api"></div>
+  </body>
+</html>
